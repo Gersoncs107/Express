3c657cfaa26740d1a072772e84a2da6e3548250f
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-15 22:14:33 -0300

    Express

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -4,4 +4,13 @@ async function getAuthorById(req, res) {
     const {authorId} = req.params
 
     const author = await db.getAuthorById(Number(authorId))
+
+    if(!author){
+        res.status(404).send("Author not found")
+        return
+    }
+
+    res.send(`Author Name: ${author.name}`)
 }
+
+module.exports = {getAuthorById}
