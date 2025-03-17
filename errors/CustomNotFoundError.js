6386f3c010358de349a54c2a8ee3bae2fbceb29b
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-16 22:16:52 -0300

    Express

--- errors/CustomNotFoundError.js
+++ errors/CustomNotFoundError.js
@@ -3,5 +3,7 @@ class CustomNotFoundError extends Error{
     constructor(message){
         super(message)
         this.statusCode = 404
+        this.name = "NotFoundError"
     }
 }
+module.exports = CustomNotFoundError
