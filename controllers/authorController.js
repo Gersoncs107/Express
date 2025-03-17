884808b8d90e1b695134df9ac4824687a66611f6
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-16 21:59:00 -0300

    Express

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,24 +1,17 @@
 const db = require("../db")
 const asyncHandler = require("express-async-handler")
 
-async function getAuthorById(req, res) {
-    const {authorId} = req.params
-
-    try {
-        const author = await db.getAuthorById(Number(authorId))
-
-    if(!author){
-        res.status(404).send("Author not found")
-        return
+const getAuthorById = asyncHandler(async (req, res) => {
+    const { authorId } = req.params;
+  
+    const author = await db.getAuthorById(Number(authorId));
+  
+    if (!author) {
+      res.status(404).send("Author not found");
+      return;
     }
-
-    res.send(`Author Name: ${author.name}`)
-    } catch (error) {
-        console.error("Error retrieving author:", error)
-        res.status(500).send("Internal Server Error")
-    }
-
-    
-}
+  
+    res.send(`Author Name: ${author.name}`);
+  });
 
 module.exports = {getAuthorById}
