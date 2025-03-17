4f01eab4bbb39ff02ba760988fa8799b0954bf88
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-16 22:28:36 -0300

    Express

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ app.listen(PORT, ()=> {
     console.log(`My first Express app - listening on port ${PORT}!`)
 })
 
-app.use((err,req, res, next) => {
-    console.error(err)
-    res.status(500).send(err)
-})
+app.use((err, req, res, next) => {
+    console.error(err);
+    // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
+    res.status(err.statusCode || 500).send(err.message);
+  });
+  
