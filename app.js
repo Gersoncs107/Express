cf3a49a0178233f22d581b111319f2996d6107e8
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-18 23:05:43 -0300

    Express

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const bookRouter = require("./routes/bookRouter")
 const indexRouter = require("./routes/indexRouter")
 const path = require("node:path")
 
-app.use("/authors", authorRouter)
-app.use("/books",bookRouter)
-app.use("/", indexRouter)
+// app.use("/authors", authorRouter)
+// app.use("/books",bookRouter)
+// app.use("/", indexRouter)
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -19,12 +19,13 @@ app.listen(PORT, ()=> {
     console.log(`My first Express app - listening on port ${PORT}!`)
 })
 
+app.get("/", (req, res) => {
+    res.render("index", {message: "EJS Rocks!"} )
+})  
+
 app.use((err, req, res, next) => {
     console.error(err);
     // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
     res.status(err.statusCode || 500).send(err.message);
   });
 
-app.get("/", (req, res) => {
-    res.render("index", {message: "EJS Rocks!"} )
-})  
